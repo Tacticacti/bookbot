5eef172a9cd788a1baa5d9093773f3d62723d211
Tacticacti <edwardoh@tudelft.nl>
2025-11-28 22:40:41 +0100

    Added a report feature

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from stats import count_words, get_book_text, count_characters
+from stats import count_words, get_book_text, count_characters, dict_to_sorted_list
 
 def main(): 
-    num_words = count_words(get_book_text("books/frankenstein.txt"))
+    print("============ BOOKBOT ============")
+    file = "books/frankenstein.txt"
+    print(f"Analyzing book found at {file}...")
+    text = get_book_text(file)
+    print("----------- Word Count ----------")
+    num_words = count_words(text)
     print(f"Found {num_words} total words")
-    char_counts = count_characters(get_book_text("books/frankenstein.txt"))
-    print(char_counts)
+    print("--------- Character Count -------")
+    char_counts = count_characters(text)
+    sorted_char_counts = dict_to_sorted_list(char_counts)
+    for item in sorted_char_counts:
+        char = item["char"]
+        count = item["count"]
+        print(f"{char}: {count}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -10,8 +10,21 @@ def count_characters(text):
     lower_text = text.lower()
     char_count = {}
     for char in lower_text:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
+        if char.isalpha():
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
     return char_count
+
+def sort_on(items):
+    return items["count"]
+
+def dict_to_sorted_list(char_count):
+    sorted_list = []
+    for key in char_count:
+        value = char_count[key]
+        new_dict = {"char": key, "count": value}
+        sorted_list.append(new_dict)
+    sorted_list.sort(key=sort_on, reverse=True)
+    return sorted_list
