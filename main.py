7fd4638b4a71e904b88900a6a7f6b71f4a3470f5
Tacticacti <edwardoh@tudelft.nl>
2025-11-28 22:24:26 +0100

    Separated function code from main code

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def get_book_text(file_path):
-    with open(file_path) as f:
-        return f.read()
-
-def count_words(text):  
-    words = text.split()
-    return len(words)
+from stats import count_words, get_book_text
 
 def main(): 
     num_words = count_words(get_book_text("books/frankenstein.txt"))
---
+++ stats.py
@@ -0,0 +1,7 @@
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+def count_words(text):  
+    words = text.split()
+    return len(words)
